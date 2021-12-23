7dc3f5f3787d0f3f50c6cd45003637c8af751b60
Aung Myo Tun <aaungaungmyotun@gmail.com>
2021-12-23 14:04:48 +0630

    Person Controller

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
